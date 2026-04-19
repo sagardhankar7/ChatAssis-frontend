92a31ea1de35f1fcf8313621173dc94a7d768bf9
Sagar Dhankar <sag9315@gmail.com>
2026-04-19 21:58:57 +0530

    Version:1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react"
 import axios from "axios"
 import ReactMarkdown from "react-markdown"
-import remarkGfm from "remark-gfm";
-
+import remarkGfm from "remark-gfm"
 
 function App() {
   const [text, setText] = useState("")
@@ -21,6 +20,15 @@ function App() {
     }
   }
 
+  const handleEnterBtn = async() => {
+    if (text.trim() == "") return
+      setOutput("")
+      const response = await axios.post("http://localhost:3001/chat", {
+        userPrompt: text,
+      })
+      setOutput(response.data?.message)
+  }
+
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
@@ -28,7 +36,9 @@ function App() {
           <div className="w-456">
             <h1 className="text-5xl font-bold">Chat Assist</h1>
             <div className="py-6 h-120 overflow-y-auto whitespace-pre-line border rounded p-2 my-3 border-gray-200">
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>{output}</ReactMarkdown>
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {output}
+              </ReactMarkdown>
             </div>
             <div className="flex justify-center">
               <input
@@ -40,7 +50,7 @@ function App() {
                 className="input w-[calc(570px)] outline-none border-none"
               />
               <div className="flex flex-col justify-center ml-1">
-                <span> Press Enter</span>
+                <button onClick={handleEnterBtn} className="btn border-gray-300 rounded-[7px] outline-none"> Enter</button>
               </div>
             </div>
           </div>
