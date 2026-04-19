4821fa84935652c25ee80eab788477b3ce524178
Sagar Dhankar <sag9315@gmail.com>
2026-04-19 19:56:05 +0530

    1: INITIAL COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import axios from "axios"
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm";
+
+
+function App() {
+  const [text, setText] = useState("")
+  const [output, setOutput] = useState("")
+
+  const handleKey = async (e) => {
+    if (e.key != "Enter") return
+    else {
+      if (text.trim() == "") return
+      setOutput("")
+      const response = await axios.post("http://localhost:3001/chat", {
+        userPrompt: text,
+      })
+      setOutput(response.data?.message)
+      //   console.log(response.data?.message)
+    }
+  }
+
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="w-456">
+            <h1 className="text-5xl font-bold">Chat Assist</h1>
+            <div className="py-6 h-120 overflow-y-auto whitespace-pre-line border rounded p-2 my-3 border-gray-200">
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>{output}</ReactMarkdown>
+            </div>
+            <div className="flex justify-center">
+              <input
+                onKeyUp={handleKey}
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Ask Anything"
+                className="input w-[calc(570px)] outline-none border-none"
+              />
+              <div className="flex flex-col justify-center ml-1">
+                <span> Press Enter</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
